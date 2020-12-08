4935add988f1d6b2c3c7d890f0aa631da7ce7888
Marcelo Ludovico <maludovico@msn.com>
2020-12-08 20:04:58 -0300

    Initial commit

---
+++ app/src/main/java/br/com/mludovico/notes/NoteDao.java
@@ -0,0 +1,18 @@
+package br.com.mludovico.notes;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+    @Query("INSERT INTO notes (contents) VALUES ('New Note')")
+    void create();
+
+    @Query("SELECT * FROM notes")
+    List<Note> getAllNotes();
+
+    @Query("UPDATE notes SET contents = :contents WHERE id = :id")
+    void save(String contents, int id);
+}
